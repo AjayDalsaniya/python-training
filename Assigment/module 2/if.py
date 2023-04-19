e2640698f7f48545e8aad096caaf7d1bc69b97b8
AjayDalsaniya <131144718+AjayDalsaniya@users.noreply.github.com>
2023-04-19 10:04:37 +0530

    updates

---
+++ Assigment/module 2/if.py
@@ -0,0 +1,9 @@
+"""Write a Python program to check if a number is positive, negative or
+zero"""
+
+num = int(input("Enter number : "))
+if num >= 0:
+    print("Positive number ")
+    
+else:
+    print("Negative number")
---
+++ Assigment/module 2/vowel.py
@@ -0,0 +1,5 @@
+letter = input("Enter letter ")
+if letter in 'aeiou':
+    print("vowel")
+else:
+    print("not vovel")
